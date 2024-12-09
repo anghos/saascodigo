eea27a4984a35612042cb5052612f0c20cfb39fe
anghos <86963297+anghos@users.noreply.github.com>
2024-12-09 10:28:39 -0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -37,7 +37,7 @@ const App = () => {
                 },
                 "&::-webkit-scrollbar-thumb": {
                     boxShadow: 'inset 0 0 6px rgba(0, 0, 0, 0.3)',
-                    backgroundColor: "#0ca95f",
+                    backgroundColor: "#000080",
                 },
             },
             scrollbarStylesSoft: {
